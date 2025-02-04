c32958c16c9e1ecddc2a1e638be123d7fb376957
Marc Vanoverschelde <marcvanouch@gmail.com>
2025-02-04 13:07:30 +0100

    Update settings.py

--- RENDER/Stock-Trading-Bot-main/src/cfehome/settings.py
+++ RENDER/Stock-Trading-Bot-main/src/cfehome/settings.py
@@ -16,7 +16,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
